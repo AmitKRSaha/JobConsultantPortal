68785cedfcb7331cb1cee711692c37822b06c384
LL\amisaha <amisaha@publicisgroupe.net>
2018-09-19 12:26:19 +0530

    Wrote Unit Test for Job Service

--- JobConsultantPortal/src/app/jobdata/job.service.spec.ts
+++ JobConsultantPortal/src/app/jobdata/job.service.spec.ts
@@ -6,6 +6,20 @@ import { TestBed } from '@angular/core/testing';
 
 import { JobService } from './job.service';
 
+import { defer } from 'rxjs';
+
+/** Create async observable that emits-once and completes
+ *  after a JS engine turn */
+export function asyncData<T>(data: T) {
+  return defer(() => Promise.resolve(data));
+}
+
+/** Create async observable error that errors
+ *  after a JS engine turn */
+export function asyncError<T>(errorObject: any) {
+  return defer(() => Promise.reject(errorObject));
+}
+
 describe('JobService', () => {
   let service: JobService;
   let httpMock: HttpTestingController;
@@ -58,6 +72,43 @@ describe('JobService', () => {
       });
       httpMock.verify();
     });
+  });
+
+});
+
+describe ('JobService (with spies)', () => {
+  let httpClientSpy: { get: jasmine.Spy };
+  let jobService: JobService;
+
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    jobService = new JobService(<any> httpClientSpy);
+  });
+
+  it('should return expected Jobs (HttpClient called once)', () => {
+    const expectedJobs: any[] =
+      [{ id: 1, name: 'A' }, { id: 2, name: 'B' }];
+
+    httpClientSpy.get.and.returnValue(asyncData(expectedJobs));
+
+    jobService.getAllJobs().subscribe(
+      jobs => expect(jobs).toEqual(expectedJobs, 'expected jobs'),
+      fail
+    );
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+  });
+
+  it('should return expected Jobs (HttpClient called once)', () => {
+    const expectedJobs: any[] =
+      [{ id: 1, name: 'A' }];
+
+    httpClientSpy.get.and.returnValue(asyncData(expectedJobs));
+
+    jobService.getJobs('1').subscribe(
+      jobs => expect(jobs).toEqual(expectedJobs, 'expected jobs'),
+      fail
+    );
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
   });
 
 });
